2a9e5dc62d6184abf57352145d0d39f177ab9440
Md Rashid <118841846+rashidrehan12@users.noreply.github.com>
2025-07-16 21:17:58 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import FacialExpression from "./components/FacialExpression"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <FacialExpression />
+    </>
+  )
+}
+
+export default App 
